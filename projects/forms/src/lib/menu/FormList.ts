033990385b083b8b35900189571c62119e4fa0ca
Alex Høffner <alex@hoeffner.net>
2021-02-28 22:55:32 +0100

    .

--- projects/forms/src/lib/menu/FormList.ts
+++ projects/forms/src/lib/menu/FormList.ts
@@ -252,7 +252,7 @@ export class FormList implements AfterViewInit
 
 		if (level > 0) html += this.pre(last[last.length-1]);
 
-		//html += "<img id='"+form.def.path+"-img' src='/assets/open.jpg'>\n";
+		//html += "<img id='"+form.def.path+"-img' src='/assets/form.jpg'>\n";
 		html += "<span id='"+form.def.path+"-lnk' class='link'> "+form.def.name+"</span>\n";
 		html += "</div>\n";
 
